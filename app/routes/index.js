1d3cf289ed373ba9cd10e5aba9c1d519d0efb8ad
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2017-10-03 22:32:31 -0500

    Add Documentation

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,44 +2,60 @@ const express = require('express');
 const router = express.Router();
 const session = require('express-session');
 const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
+const {encryptUserPassword} = require('../helpers/users');
 
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
   next()
 })
 
-// define the home page route
+// home page route
 router.get('/', function (req, res) {
   res.send('Birdie OP home page')
 })
-// define the login route
+
+// login route
 router.post('/login', function (req, res) {
   let email = req.body.email;
-  // let password = req.body.password;
-  User.findOne({email:email}, function(err, user){
+
+  /**
+    * Finds the the users email
+    * @param {email} x - users email
+    */
+  User.findOne({email: email}, function(err, user){
     if(err){
       return res.status(500).send('log in failed');
     }
-
     if(!user){
       return res.status(404).send('No user found');
     }
     req.session.user = user;
-    return res.status(200).send('You have been logged in');
+    let password = req.body.password;
+    /**
+      * Checks if the users plain text password is equaled to the hash version.
+      * @param {password} x - users plain text password
+      * @param {session_password_hash} x - users hash password
+      */
+    bcrypt.compare(password, req.session.user.password_hash, function(err, res2) {
+        if(res2 == true){
+          return res.status(200).send('You have been logged in');
+        }else{
+          return res.status(500).send('Email or Password is incorrect');
+        }
+    });
   })
-
 })
 
-// define the logout route
+// logout route
 router.get('/logout', function (req, res) {
   req.session.destroy(function(err) {
   })
   res.send('You have been logout')
 })
 
-// define the dashboard route
+// dashboard route
 router.get('/dashboard', function (req, res) {
-  // res.send('dashboard')
   if(!req.session.user){
     return res.status(404).send('Sign in to view desktop');
   }
